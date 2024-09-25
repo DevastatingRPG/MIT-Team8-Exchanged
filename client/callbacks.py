7851277074031d9bd82816f73eff8cec4c79b062
Shubham Sarkar <shubham04sarkar@gmail.com>
2024-09-25 19:05:33 +0530

    added the callbacks for data

--- client/callbacks.py
+++ client/callbacks.py
@@ -1,6 +1,7 @@
 from dash import callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import polars as pl
 from dotenv import load_dotenv, find_dotenv
 import os
 import requests
@@ -9,39 +10,41 @@ from datetime import datetime
 # Load environment variables from .env file
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
-print(f"Loaded dotenv path: {dotenv_path}")
+# print(f"Loaded dotenv path: {dotenv_path}")
 
 # Access the API key
 api_key = os.getenv('API_KEY')
-print(api_key)
+# print(api_key)
 base_url = f'https://openexchangerates.org/api'
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-def get_fx_rates(base_currency="USD", date=None):
-    date_obj = datetime.strptime(date, '%Y-%m-%d')
-    today_date = datetime.today().strftime('%Y-%m-%d')
-
-    if date == today_date or date == None:
-        url = base_url + f"/latest.json?app_id={api_key}"
-    else:
-        url = base_url + f"/historical/{date}.json?app_id={api_key}"
-
+def fetchData(url):
     headers = {"accept": "application/json"}
-    print(date)
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
         response.raise_for_status()
 
+def get_fx_rates(base_currency="USD", date=None):
+    today_date = datetime.today().strftime('%Y-%m-%d')
+
+    if date == today_date or date == None:
+        url = base_url + f"/latest.json?app_id={api_key}"
+    else:
+        url = base_url + f"/historical/{date}.json?app_id={api_key}"
+    rates = fetchData(url)['rates']
+    table_data = [{'Currency': currency, 'Rate': rate} for currency, rate in rates.items()]
+    table_df = pd.DataFrame(table_data)
+    return table_df
+
 def get_currencies():
-    url = base_url = '/currencies.json'
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        response.raise_for_status()
+    url = base_url + '/currencies.json'
+    currencies = fetchData(url)
     rates = get_fx_rates()
+    rates['Description'] = rates['Currency'].map(currencies)
+    return rates
     
     
         
@@ -60,8 +63,8 @@ def register_callbacks(app):
         [Input('base-currency-dropdown', 'value'), Input('date-picker-single', 'date')]
     )
     def update_fx_graph(base, date):
-        print('hi')
         rates = get_fx_rates(base, date)
-        print(rates)
-        table_data = [{'Currency': currency, 'Rate': rate} for currency, rate in rates['rates'].items()]
-        return table_data
+    
+        get_currencies()
+        # print(get_currencies())
+        return rates.to_dict(orient='records')
