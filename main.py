6cdc32165890d52153171c24697710fb79bde3a0
Shubhankar Salunke <shubhankar.salunke@gmail.com>
2024-09-26 10:42:56 +0530

    added risk and custom basket functionality

--- main.py
+++ main.py
@@ -4,6 +4,46 @@ import numpy as np
 import plotly.express as px
 from streamlit_option_menu import option_menu
 from pymongo import MongoClient
+from datetime import date, datetime
+import re
+import polars as pl
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+base_url = f"https://openexchangerates.org/api"
+
+
+def fetchData(url):
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def get_fx_rates(base_currency="USD", date=None):
+    today_date = datetime.today().strftime("%Y-%m-%d")
+
+    if date == today_date or date == None:
+        url = base_url + f"/latest.json?app_id={api_key}"
+    else:
+        url = base_url + f"/historical/{date}.json?app_id={api_key}"
+    rates = fetchData(url)["rates"]
+    rates_df = pl.DataFrame(
+        {"Currency": list(rates.keys()), "Rate": list(rates.values())}
+    )
+    return rates_df
+
+def extract_currency_abbreviation(column_name):
+    match = re.search(r"\((.*?)\)", column_name)
+    if match:
+        return match.group(1)
+    return column_name
 
 def render_footer():
     footer_html = """
@@ -71,25 +111,21 @@ for collection_name in collections:
 
 df = pd.concat(dataframes, ignore_index=True)
 
-def calculate_hv(df, currency_col, period):
+def calculate_hv(df, currency_col, period='monthly'):
     df = df[['Date', currency_col]].dropna()
     df = df.set_index('Date')
 
+    # Calculate returns based on selected period
     if period == 'monthly':
         returns = df[currency_col].resample('M').last().pct_change()
     elif period == 'quarterly':
         returns = df[currency_col].resample('Q').last().pct_change()
-    elif period == 'weekly':
-        returns = df[currency_col].resample('W').last().pct_change()
-    else:
-        returns = pd.DataFrame()
+    elif period == 'yearly':
+        returns = df[currency_col].resample('Y').last().pct_change()
 
-    if returns.empty:
-        return None, pd.DataFrame()
+    returns = returns.dropna()
 
-    # Calculate volatility as the standard deviation of returns
     volatility = returns.std() * np.sqrt(len(returns))
-
     return volatility, returns
 
 def modify_data(df):
@@ -98,11 +134,7 @@ def modify_data(df):
 
 exchange_rate_data = modify_data(df)
 
-# def resample_data(df, currency_col, frequency):
-#     sumofnulls = df[['Date', currency_col]].isnull().sum()
-#     resampled_df = df[['Date', currency_col]].dropna()
-#     resampled_df = resampled_df.resample(frequency, on='Date').mean().reset_index()
-#     return resampled_df,sumofnulls
+
 
 selected = option_menu(
     menu_title=None,
@@ -135,13 +167,11 @@ if selected == "Exchange Rate Dashboard":
 
     resampled_data['Currency 1 / Currency 2'] = resampled_data[currency_1] / resampled_data[currency_2]
 
-    # Plotting the detailed chart
     fig = px.line(
         resampled_data, x='Date', y='Currency 1 / Currency 2',
         title=f'{currency_1} <br >                  vs <br>{currency_2} <br>Exchange Rate Trend', markers=True
     )
 
-    # Customize layout
     fig.update_layout(
         xaxis_title='Date',
         yaxis_title=f'{currency_1} <br>in terms of {currency_2}',
@@ -154,7 +184,7 @@ if selected == "Exchange Rate Dashboard":
         hovermode='x unified',  # Unified hover effect
     )
 
-    # Add annotations for highest and lowest rates
+    # annotations for highest and lowest rates
     if not resampled_data.empty:
         highest_rate = (resampled_data[currency_1] / resampled_data[currency_2]) .max()
         highest_rate_date = resampled_data[(resampled_data[currency_1] / resampled_data[currency_2]) == highest_rate]['Date'].iloc[0]
@@ -174,26 +204,39 @@ if selected == "Exchange Rate Dashboard":
         st.write(f"**Highest Rate**: {highest_rate} on {highest_rate_date.date()}")
         st.write(f"**Lowest Rate**: {lowest_rate} on {lowest_rate_date.date()}")
 
+    null_count_currency_1 = exchange_rate_data[currency_1].isnull().sum()
+    if null_count_currency_1 != 0:
+        st.warning(f"Note : There are {null_count_currency_1} null values for {currency_1} which are not printed on the graph")
+
 
 elif selected == "Custom Currency Basket":
     st.title("Custom Currency Basket")
 
-
-    basket_size = st.number_input("Enter Basket Size:", min_value=1, max_value=1000, value=100, step=1)
+    year = st.number_input(
+        "Enter year: ", min_value=2000, max_value=2024, step=1, value=2024
+    )
+    date = date(year, 1, 1)
+    basket_size = st.number_input(
+        "Enter Basket Size:", min_value=1, max_value=1000, value=100, step=1
+    )
 
     base_currency = st.selectbox("Select Base Currency", exchange_rate_data.columns[1:])
 
-    basket_currencies = st.multiselect("Select Currencies for Basket", exchange_rate_data.columns[1:])
-    
+    basket_currencies = st.multiselect(
+        "Select Currencies for Basket", exchange_rate_data.columns[1:]
+    )
+
     basket_weights = {}
-    
+
     for currency in basket_currencies:
-        basket_weights[currency] = st.slider(f"Weight of {currency}:", 0, 100, 50)
+        basket_weights[currency] = st.slider(f"Weight of {currency}:", 0, 100, 100)
 
     total_weight = sum(basket_weights.values())
 
     if total_weight > 100:
         st.error("Total weight exceeds 100%. Please adjust the weights.")
+    elif total_weight < 100:
+        st.error("Total weight does not add up to 100%. Please adjust the weights.")
     else:
         st.success(f"Total weight: {total_weight}%")
         st.success(f"Total Basket Size: {basket_size}")
@@ -205,6 +248,17 @@ elif selected == "Custom Currency Basket":
 
         st.write(f"Base Currency for the basket: {base_currency}")
 
+        rates = get_fx_rates(base_currency, date)
+        rates = rates.to_pandas()
+        total_value = 0.0
+        for currency, weight in basket_weights.items():
+            abbr = extract_currency_abbreviation(currency)
+            value = rates.loc[rates["Currency"] == abbr, "Rate"]
+            value = value.iloc[0]
+            weight = (weight / 100) * basket_size
+            total_value += weight * value
+        st.write(f"Value in base currency: {total_value}")
+
 
 elif selected == "Risk Factor":
     st.title("Risk Factor Analysis - Historic Volatility")
@@ -212,39 +266,50 @@ elif selected == "Risk Factor":
     currencies = exchange_rate_data.columns[1:]  # Get all available currencies
     currency_selected = st.selectbox("Select a Currency for HV Calculation:", currencies)
 
-    date_selections = []
-    year_selected = st.selectbox("Select Year for Volatility Analysis:", sorted(exchange_rate_data['Date'].dt.year.unique()))
+    year_selected = st.selectbox("Select Year for Volatility Analysis:", exchange_rate_data['Date'].dt.year.unique())
 
     filtered_data = exchange_rate_data[exchange_rate_data['Date'].dt.year == year_selected]
 
-    period_selected = st.selectbox("Select the Period:", ["monthly", "quarterly", "weekly"])
+    period_selected = st.selectbox("Select the Period:", ["monthly", "quarterly", "yearly"])
 
     # Calculating HV
-    hv, returns = calculate_hv(filtered_data, currency_selected, period_selected)
+    hv, returns = calculate_hv(filtered_data, currency_selected, period=period_selected)
 
     if hv is None or returns.empty:
-        st.write(f"No sufficient data to calculate historical volatility for {currency_selected} in {year_selected}.")
+        st.write(f"Data is missing or insufficient to calculate historical volatility for {currency_selected} in {year_selected}.")
     else:
-        st.write(f"**Annualized Historical Volatility** for {currency_selected} in {year_selected} is: {hv:.2%}")
+        if hv < 0.10:
+            volatility_level = "Low Risk"
+            line_color = "green"
+        elif 0.10 <= hv < 0.20:
+            volatility_level = "Medium Risk"
+            line_color = "orange"
+        else:
+            volatility_level = "High Risk"
+            line_color = "red"
 
-        # Plot returns only if available
+        st.write(f"**Risk Level** for {currency_selected} in {year_selected} is: {volatility_level}")
+
         if not returns.empty:
+            # Create the figure with the line color set based on volatility
             fig = px.line(
                 x=returns.index,
                 y=returns.values,
-                title=f'Returns for {currency_selected} <br>            ({period_selected.capitalize()} data) in {year_selected}',
+                title=f'Returns for {currency_selected} ({period_selected.capitalize()} data) in {year_selected}',
                 labels={'x': 'Date', 'y': 'Returns'},
                 markers=True
             )
 
+            fig.update_traces(line=dict(color=line_color))
+
             fig.update_layout(
                 xaxis_title='Date',
                 yaxis_title='Returns',
-                title_x=0.5,  # Center the title
+                title_x=0.5,
                 showlegend=False,
-                xaxis=dict(showgrid=True),  # Show gridlines on x-axis
-                yaxis=dict(showgrid=True),  # Show gridlines on y-axis
-                hovermode='x unified',  # Unified hover effect
+                xaxis=dict(showgrid=True),
+                yaxis=dict(showgrid=True),
+                hovermode='x unified',
             )
 
             st.plotly_chart(fig)
